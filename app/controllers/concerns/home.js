945530ef0797cb6f2a5f30fa1146cff17fafadf9
anhuirong <ahr304@qq.com>
2013-12-24 14:42:09 +0800

    Finish layout and routes

---
+++ app/controllers/concerns/home.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: git
+ * Date: 13-8-10
+ * Time: 下午6:23
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '2a9934a345f6936321287a15deb04d40765ee382fb1c31362e873e372207683735d0407119404ecdb292d116281fb1af7bb9809e5614034b57046e38c41bb7c8'
